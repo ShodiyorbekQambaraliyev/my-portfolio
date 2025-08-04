c7cbbb3f42874379f7164210967044f75e177ef9
ShodiyorbekQambaraliyev <shodiyorbek057@gmail.com>
2025-08-04 17:31:11 +0500

    home sahifa menuga about_me sahifasi homega ozgardi va yangi about_me sahifasi qoshildi

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,8 @@ from  django.urls import path
 from  .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('about-me/', about_me, name='about-me'),
+    path('', menu, name='menu'),
+    path('home/', home, name='home'),
     path('my_projects/', my_projects, name='my_projects'),
+    path('about_me/', about_me, name='about_me'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
 
+def menu(request):
+    return render(request, 'menu.html')
+
 def home(request):
     return render(request, 'home.html')
 
-def about_me(request):
-    return render(request, 'about_me.html')
-
 def my_projects(request):
     return render(request, 'my_projects.html')
+
+def about_me(request):
+    return render(request, 'about_me.html')

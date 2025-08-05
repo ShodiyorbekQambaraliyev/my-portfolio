34a9b136d25faec3cd22ccb198e4303c7b64dc79
ShodiyorbekQambaraliyev <shodiyorbek057@gmail.com>
2025-08-05 14:22:53 -0300

    home bolimi toliq bitdi faqat skilsni backend tomonida chala joy bor frontend va backend tomonlari yozildi

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from .models import Menu
+from .models import Menu, Home
 
 def menu(request):
     menu = Menu.objects.all()
     return render(request, 'menu.html', {'menu': menu})
 
 def home(request):
-    return render(request, 'home.html')
+    home = Home.objects.all()
+    return render(request, 'home.html', {'home': home})
 
 def my_projects(request):
     return render(request, 'my_projects.html')
